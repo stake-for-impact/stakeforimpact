2cf0a8ffd65032125b8e1e27818e02c5e7f284c6
Adam Sobotka <vorcigernix@gmail.com>
2023-06-11 19:04:38 +0200

    detailpage

--- src/routes/detail.tsx
+++ src/routes/detail.tsx
@@ -22,68 +22,52 @@ export default function Detail() {
 
   return (
     data && (
-      <section className="w-full">
-        <div className="relative items-center w-full px-5 pt-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
-          <div className="flex w-full mx-auto text-left">
-            <div className="relative flex-col items-center mx-auto">
-              <div className="inline-flex items-center text-left w-full">
-                <div className="relative items-center mx-auto lg:inline-flex">
-                  <div className="items-start text-left lg:inline-flex">
-                    <div className="lg:pr-16">
-                      <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 lg:text-5xl lg:max-w-7xl">
-                        {data![0]}
-                      </h1>
-                    </div>
-                    <div>
-                      <p className="max-w-md mt-4 text-base text-gray-500 md:max-w-xl lg:mt-0">
-                        {data![1]}
-                      </p>
-                      <form
-                        action=""
-                        method="post"
-                        id="revue-form"
-                        name="revue-form"
-                        target="_blank"
-                        className="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-gray-50 rounded-xl sm:max-w-lg sm:flex"
-                      >
-                        <div className="flex-1 min-w-0 revue-form-group">
-                          <label htmlFor="stakeamount" className="sr-only">
-                            amount to stake
-                          </label>
-                          <input
-                            id="stakeamount"
-                            type="email"
-                            className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                            placeholder="Enter amount to stake  "
-                          />
-                        </div>
-                        <div className="mt-4 sm:mt-0 sm:ml-3 revue-form-actions">
-                          <button
-                            type="submit"
-                            value="Subscribe"
-                            name="member[subscribe]"
-                            id="member_submit"
-                            className="block w-full px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 sm:px-10"
-                          >
-                            Stake
-                          </button>
-                        </div>
-                      </form>
+      <>
+        <section>
+          <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+            <div className="flex flex-wrap items-center mx-auto max-w-7xl">
+              <div className="w-full lg:max-w-lg lg:w-1/2 rounded-xl">
+                <div>
+                  <div className="relative w-full max-w-lg">
+                    <div className="absolute top-0 rounded-full bg-violet-300 -left-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+
+                    <div className="absolute rounded-full bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+                    <div className="relative">
+                      <img className="object-cover object-center mx-auto rounded-lg shadow-2xl aspect-square opacity-50" alt="hero" src="https://source.unsplash.com/random/700×700/?people" />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col items-center justify-center py-24 mx-auto rounded-lg max-w-7xl">
-                <img
-                  className="object-cover object-center w-full rounded-xl"
-                  alt="hero"
-                  src="https://source.unsplash.com/random/900×700/?people"
-                />
+              <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+                <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase"> Support </span>
+                <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl"> {data![0]}.</h1>
+                <p className="mb-8 text-base leading-relaxed text-left text-gray-500"> {data![1]}</p>
+                <div className="flex-col mt-0 lg:mt-6 max-w-7xl sm:flex">
+                  <form action="" method="post" id="revue-form" name="revue-form" target="_blank" className="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-gray-50 rounded-xl sm:max-w-lg sm:flex">
+                    <div className="flex-1 min-w-0 revue-form-group">
+                      <label htmlFor="stakeamount" className="sr-only">Stake amount</label>
+                      <input id="stakeamount" type="number" className="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md text-neutral-600 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" placeholder="Amount of stake  "/>
+                    </div>
+                    <div className="mt-4 sm:mt-0 sm:ml-3 revue-form-actions">
+                      <button type="submit" value="Subscribe" name="stake" id="stake" className="block w-full px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 sm:px-10">Stake</button>
+                    </div>
+                  </form>
+                  <div className="sm:max-w-lg sm:flex">
+                    <p className="mt-3 text-xs text-gray-500">
+                      By staking, you agree with 
+                      <a target="_blank" href="/tos">Terms of Service</a>
+                      &nbsp;and&nbsp;
+                      <a target="_blank" href="/privacy">Privacy Policy</a>.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
+
+
     )
   );
 }
