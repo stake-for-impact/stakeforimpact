05509c1900a3ba10a2469d3ccd3cb9f0bf3344c8
Adam Sobotka <vorcigernix@gmail.com>
2023-06-11 10:57:03 +0200

    mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,23 +22,23 @@ export function Navbar() {
           >
             <svg fill="currentColor" viewBox="0 0 20 20" className="w-8 h-8">
               <path
-                x-show="!open"
+
+                className={open ? `hidden` : `block`}
                 fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                 clipRule="evenodd"
               />
               <path
-                x-show="open"
                 fillRule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                 clipRule="evenodd"
-                className="hidden"
+                className={open ? `block` : `hidden`}
               />
             </svg>
           </button>
         </div>
 
-        <nav className="flex-col items-center flex-grow hidden pb-4 border-blue-600 md:pb-0 md:flex md:justify-end md:flex-row lg:border-l-2 lg:pl-2">
+        <nav className={open ? `justify-center items-center flex-grow pb-4 border-blue-600 md:pb-0 md:flex md:justify-end md:flex-row lg:border-l-2 lg:pl-2 mt-4 bg-gray-50 p-4 md:bg-transparent md:p-0 rounded-3xl md:rounded-none` : `hidden`}>
           <a
             className="px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 hover:text-blue-600 focus:outline-none focus:shadow-outline"
             href="/about"
@@ -51,8 +51,8 @@ export function Navbar() {
           >
             Contact
           </a>
-
-          <div className="inline-flex items-center gap-2 list-none lg:ml-auto">
+          <br className="md:hidden"/>
+          <div className="inline-flex items-center gap-2 list-none lg:ml-auto mt-4 md:mt-0">
             {isConnected ? (
               <Link
                 className="items-center block px-10 py-2.5 text-base font-medium text-center text-blue-600 transition duration-500 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
